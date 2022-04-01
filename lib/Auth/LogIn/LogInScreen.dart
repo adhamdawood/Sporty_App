0ed8a59bbaad11de0ae7c6b77d52dc305a759a5a
adhamdawood <adham.am758@gmail.com>
2022-04-01 15:01:45 +0200

    for u

--- lib/Auth/LogIn/LogInScreen.dart
+++ lib/Auth/LogIn/LogInScreen.dart
@@ -5,7 +5,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:sporty_app/APIs/login_response.dart';
 import 'package:sporty_app/Auth/LogIn/ForgetPasswordScreen.dart';
 import 'package:http/http.dart' as http;
-import 'package:sporty_app/Home/chooseInterests.dart';
+import 'package:sporty_app/Home/HomeScreen.dart';
 import 'package:sporty_app/Shared_preferences/Cache_Helper.dart';
 
 import '../SignUp/SignUpScreen.dart';
@@ -30,89 +30,89 @@ class _LogInScreenState extends State<LogInScreen> {
   }
   @override
   Widget build(BuildContext context) {
-        return Scaffold(
-          body: Container(
-            height: double.infinity,
-            width: double.infinity,
-            margin: const EdgeInsets.all(25),
-            child: FutureBuilder<LoginResponse>(
+        return  FutureBuilder<LoginResponse>(
                 future: loginResponse,
                 builder: (context,snapshot){
                   if (snapshot.hasData){
-                   cacheHelper.saveData(key: "token", value: snapshot.data.token);
-                    return chooseInterests();
+                   cacheHelper.saveData(key: "token", value: snapshot.data.token).then((value) => HomePage());
+                    return HomePage();
                   }else {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Image.asset("assets/images/welcomeScreen.png", fit: BoxFit.fill,),
-                        const Text("Welcome Hero, Sign In To Continue",
-                          style: TextStyle(fontSize: 14),),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            hintText: "E-mail",
-                            labelText: 'E-mail *',
-                          ),
-                          onChanged : (value) {
-                            email=value;
-                          },
-                        ),
-                        TextFormField(
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                            hintText: "Password",
-                            labelText: 'Password *',
-                          ),
-                          onChanged: (value) {
-                            password=value;
-                          },
-                        ),
-                        InkWell(
-                            onTap: () {
-                              Navigator.of(context)
-                                  .push(MaterialPageRoute(
-                                  builder: (_) => ForgetPasswordScreen()));
-                            },
-                            child: const Text("Forget Password ?", textAlign: TextAlign.end,)),
-                        Container(
-                          width: 296, height: 40,
-                          decoration: BoxDecoration(
-                            color: const Color(0xFFE20030),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: TextButton(
-                            onPressed: () {
-                              setState(() {
-                                loginResponse= LoadResponse();
-                              });
-                              onSignInButton=1;
-                            },
-                            child: const Text(
-                              "Sign In", style: TextStyle(color: Colors.white),),),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Navigator.of(context)
-                                .push(MaterialPageRoute(builder: (_) => SignUpScreen()));
-                          },
-                          child: const Text.rich(
-                            TextSpan(
-                              text: "Don't Have An Account ?", // default text style
-                              children: <TextSpan>[
-                                TextSpan(text: ' Sign Up ',
-                                  style: TextStyle(color: Color(0xFFE20030),
-                                      fontWeight: FontWeight.bold),),
-                              ],
+                    return Scaffold(
+                      body: Container(
+                        height: double.infinity,
+                        width: double.infinity,
+                        margin: const EdgeInsets.all(25),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Image.asset("assets/images/welcomeScreen.png", fit: BoxFit.fill,),
+                            const Text("Welcome Hero, Sign In To Continue",
+                              style: TextStyle(fontSize: 14),),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                hintText: "E-mail",
+                                labelText: 'E-mail *',
+                              ),
+                              onChanged : (value) {
+                                email=value;
+                              },
                             ),
-                          ),
+                            TextFormField(
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                hintText: "Password",
+                                labelText: 'Password *',
+                              ),
+                              onChanged: (value) {
+                                password=value;
+                              },
+                            ),
+                            InkWell(
+                                onTap: () {
+                                  Navigator.of(context)
+                                      .push(MaterialPageRoute(
+                                      builder: (_) => ForgetPasswordScreen()));
+                                },
+                                child: const Text("Forget Password ?", textAlign: TextAlign.end,)),
+                            Container(
+                              width: 296, height: 40,
+                              decoration: BoxDecoration(
+                                color: const Color(0xFFE20030),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: TextButton(
+                                onPressed: () {
+                                  setState(() {
+                                    loginResponse= LoadResponse();
+                                  });
+                                  onSignInButton=1;
+                                },
+                                child: const Text(
+                                  "Sign In", style: TextStyle(color: Colors.white),),),
+                            ),
+                            InkWell(
+                              onTap: () {
+                                Navigator.of(context)
+                                    .push(MaterialPageRoute(builder: (_) => SignUpScreen()));
+                              },
+                              child: const Text.rich(
+                                TextSpan(
+                                  text: "Don't Have An Account ?", // default text style
+                                  children: <TextSpan>[
+                                    TextSpan(text: ' Sign Up ',
+                                      style: TextStyle(color: Color(0xFFE20030),
+                                          fontWeight: FontWeight.bold),),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     );
                   }
                 }
-            ),
-          ),
-        );
+                );
       }
   Future<LoginResponse> LoadResponse() async {
     var body = jsonEncode({
--- lib/Home/ProfileView/ProfileViewScren.dart
+++ lib/Home/ProfileView/ProfileViewScren.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:sporty_app/Auth/LogIn/LogInScreen.dart';
+import 'package:sporty_app/Shared_preferences/Cache_Helper.dart';
 
 class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Container(
+        child: TextButton(
+          onPressed: (){
+            cacheHelper.removeData(key: "token").then((value) {
+              if(value){
+                Navigator.pushNamed(context,LogInScreen.ROUTE_NAME);
+              }
+            });
+          },
+          child: Text("sign out"),
+        ),
+      ),
+    );
   }
 }
